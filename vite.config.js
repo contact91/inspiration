7e84d357029d6c93d44ed33a002b41bf96c4a2b4
contact91 <chouvelquentin91@gmail.com>
2025-05-12 23:04:58 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // Set base path for GitHub Pages
+  server: {
+    hmr: {
+      overlay: false, // Disable the error overlay
+    },
+  },
+  build: {
+    outDir: 'dist', // Output directory for build
+  },
 });
